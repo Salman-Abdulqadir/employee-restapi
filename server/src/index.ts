3e981aeaa9151324b6ae07fd91f7e26100508211
Salman Abdulqadir <s.abdulqadir@saals-MacBook-Pro.local>
2023-07-04 09:43:45 +0400

    third

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,27 @@
 import express, { Request, Response, NextFunction } from "express";
-import { environment } from "./config";
+import mongoose from "mongoose";
 
 import routes from "./routes/routes";
-import mongoose from "mongoose";
+
+// environment variables
+import { environment } from "./config";
+
+// employee model and service
 import { EmployeeModel } from "./models/Employee";
 import EmployeeService from "./services/employee.service";
-import { generateToken, isAuth } from "./middleware/isAuth.middleware";
+
+// notification services
 import { EmailNotification } from "./services/email.service";
 import { SmsNotification } from "./services/sms.service";
 import { FaxNotification } from "./services/fax.service";
 
+// middlewares
+import { generateToken, isAuth } from "./middleware/isAuth.middleware";
+import { errorHandler } from "./middleware/error.middleware";
+
 const app = express();
 
-// getting the db url
+// getting the db url and port number environment variables
 const db_url =
   environment.DB_URL ||
   "mongodb+srv://salman:XJ0fFNbROVRE610s@cluster0.qijjvid.mongodb.net/employees?retryWrites=true&w=majority";
@@ -21,13 +30,16 @@ const port = environment.PORT || 3000;
 // body parser
 app.use(express.json());
 
+// generate token middleware
 app.use("/api/v1/generate-token", generateToken);
 
+// authentication middleware
 app.use(isAuth);
 
+// employee routes
 app.use(
   "/api/v1",
-  routes(new EmployeeService(EmployeeModel), new SmsNotification())
+  routes(new EmployeeService(EmployeeModel), new EmailNotification())
 );
 
 // 404
@@ -35,15 +47,19 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.send("<h1>404: Page Not found<h1/>").status(404);
 });
 
-(async () => {
+// error handler
+app.use(errorHandler);
+
+const start = async () => {
   try {
     if (db_url === "") throw "The database link is empty!";
     await mongoose.connect(db_url);
-
-    app.listen(port, () => {
-      console.log(`Server started on port ${port}`);
-    });
   } catch (err) {
     console.log(err);
   }
-})();
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+};
+
+start();
