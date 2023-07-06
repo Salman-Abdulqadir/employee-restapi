525362d2e359d3edf31b151af28081c3d9ea2fce
Salman Abdulqadir <s.abdulqadir@saals-MacBook-Pro.local>
2023-07-06 16:24:59 +0400

    refactoring

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import * as swaggerDocument from "./swagger.json";
+
+// swagger
+import swaggerUi from "swagger-ui-express";
+
+import routes from "./routes/routes";
+
+// environment variables
+import { environment } from "./config";
+
+// employee model and service
+import { EmployeeModel } from "./models/Employee";
+import EmployeeService from "./services/employee.service";
+
+// middlewares
+import { generateToken, isAuth } from "./middleware/isAuth.middleware";
+import { errorHandler } from "./middleware/error.middleware";
+
+const app = express();
+
+// getting the db url and port number environment variables
+const db_url = environment.DB_URL || "";
+const port = environment.PORT || 3000;
+
+// body parser
+app.use(express.json());
+
+// generate token middleware
+app.use("/api/v1/generate-token", generateToken);
+
+//documentation
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// authentication middleware
+// app.use(isAuth);
+
+// employee routes
+app.use("/api/v1", routes(new EmployeeService(EmployeeModel)));
+
+// 404
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.send("<h1>404: Page Not found<h1/>").status(404);
+});
+
+// error handler
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    if (db_url === "") throw "The database link is empty!";
+    await mongoose.connect(db_url);
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+};
+
+start();
---
+++ src/routes/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import EmployeeController from "../controllers/employeeController";
+import EmployeeService from "../services/employee.service";
+import { NotificationHandler } from "../notification/notificationHandler";
+
+export default (_employeeService: EmployeeService) => {
+  const router = Router();
+
+  const employeeController = new EmployeeController(_employeeService);
+  employeeController.attach(new NotificationHandler());
+
+  router.route("/employee").get(employeeController.getAllEmployees);
+  router.post("/employee", employeeController.addEmployee);
+  router.patch("/employee", employeeController.updateEmployee);
+  router.delete("/employee/:id", employeeController.deleteEmployee);
+
+  return router;
+};
